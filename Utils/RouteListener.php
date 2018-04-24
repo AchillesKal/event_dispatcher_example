7d282205862f1763429904e234bcff312406e1bd
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-04-24 20:03:53 +0300

    fixes event listener

--- Utils/RouteListener.php
+++ Utils/RouteListener.php
@@ -3,11 +3,19 @@
 namespace Drupal\eventdispatcherexample\Utils;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class RouteListener implements EventSubscriberInterface
 {
+    public function onKernelRequest($event)
+    {
+        var_dump($event);die;
+    }
+
     public static function getSubscribedEvents()
     {
-        // TODO: Implement getSubscribedEvents() method.
+        return [
+            KernelEvents::REQUEST => 'onKernelRequest',
+        ];
     }
 }
