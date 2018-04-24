17b3ba930a0f1c23a3efd646d90689bc278e2545
Achilles Kaloeridis <achilleas90gr@gmail.com>
2018-04-24 19:58:13 +0300

    adds routing listener

---
+++ Utils/RouteListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\eventdispatcherexample\Utils;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class RouteListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        // TODO: Implement getSubscribedEvents() method.
+    }
+}
